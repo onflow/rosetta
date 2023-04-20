eeb25b4706b27c11506fa1a6a01f6817a227a78f
Amlandeep Bhadra <amlandeep1912@gmail.com>
2023-04-20 15:18:08 -0400

    reproduce issue

--- state/convert_test.go
+++ state/convert_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/onflow/rosetta/access"
 	"github.com/onflow/rosetta/config"
 	"github.com/stretchr/testify/assert"
 )
@@ -13,7 +14,7 @@ func TestVerifyBlockHash(t *testing.T) {
 	var notWorkingBlockHeight uint64 = 50767887
 	var workingBlockHeight uint64 = 50767885
 	ctx := context.Background()
-	spork, err := createSpork()
+	spork, err := createSpork(ctx)
 	if err != nil {
 		assert.Fail(t, err.Error())
 	}
@@ -38,6 +39,14 @@ func TestVerifyBlockHash(t *testing.T) {
 	assert.True(t, verifyBlockHash(spork, notWorkingBlock.Id, notWorkingBlockHeight, notWorkingBlockhdr, notWorkingBlock))
 }
 
-func createSpork() (*config.Spork, error) {
-	return nil, nil
+func createSpork(ctx context.Context) (*config.Spork, error) {
+	addr := "access-001.mainnet22.nodes.onflow.org:9000"
+	pool := access.New(ctx, []access.NodeConfig{{Address: addr}}, nil)
+	chain := &config.Chain{Network: "mainnet-22"}
+	return &config.Spork{
+		Version:     5,
+		Chain:       chain,
+		AccessNodes: pool,
+		RootBlock:   47169687,
+	}, nil
 }
