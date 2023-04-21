03e7849e60cdbc00a2265796004499d852c2bef5
Amlandeep Bhadra <amlandeep1912@gmail.com>
2023-04-21 14:06:11 -0400

    add version 6 logic

--- state/convert_test.go
+++ state/convert_test.go
@@ -11,38 +11,31 @@ import (
 
 func TestVerifyBlockHash(t *testing.T) {
 	// load mainnet config and get blocks exactly as state.go
-	var notWorkingBlockHeight uint64 = 50767887
-	var workingBlockHeight uint64 = 50767885
+	var startBlockHeight uint64 = 50767887
+	var endBlockHeight uint64 = 50767897
 	ctx := context.Background()
 	spork, err := createSpork(ctx)
 	if err != nil {
 		assert.Fail(t, err.Error())
 	}
 	client := spork.AccessNodes.Client()
-	workingBlock, err := client.BlockByHeight(ctx, workingBlockHeight)
-	if err != nil {
-		assert.Fail(t, err.Error())
+	for blockHeight := startBlockHeight; blockHeight < endBlockHeight; blockHeight++ {
+		block, err := client.BlockByHeight(ctx, blockHeight)
+		if err != nil {
+			assert.Fail(t, err.Error())
+		}
+		blockHeader, err := client.BlockHeaderByHeight(ctx, blockHeight)
+		if err != nil {
+			assert.Fail(t, err.Error())
+		}
+		assert.True(t, verifyBlockHash(spork, block.Id, blockHeight, blockHeader, block))
 	}
-	notWorkingBlock, err := client.BlockByHeight(ctx, notWorkingBlockHeight)
-	if err != nil {
-		assert.Fail(t, err.Error())
-	}
-	workingBlockhdr, err := client.BlockHeaderByHeight(ctx, workingBlockHeight)
-	if err != nil {
-		assert.Fail(t, err.Error())
-	}
-	notWorkingBlockhdr, err := client.BlockHeaderByHeight(ctx, notWorkingBlockHeight)
-	if err != nil {
-		assert.Fail(t, err.Error())
-	}
-	assert.True(t, verifyBlockHash(spork, workingBlock.Id, workingBlockHeight, workingBlockhdr, workingBlock))
-	assert.True(t, verifyBlockHash(spork, notWorkingBlock.Id, notWorkingBlockHeight, notWorkingBlockhdr, notWorkingBlock))
 }
 
 func createSpork(ctx context.Context) (*config.Spork, error) {
 	addr := "access-001.mainnet22.nodes.onflow.org:9000"
 	pool := access.New(ctx, []access.NodeConfig{{Address: addr}}, nil)
-	chain := &config.Chain{Network: "mainnet-22"}
+	chain := &config.Chain{Network: "mainnet"}
 	return &config.Spork{
 		Version:     5,
 		Chain:       chain,
