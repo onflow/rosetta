038434a83e2bd22f63de754cd500261f1074dcf3
Amlandeep Bhadra <koko1123@users.noreply.github.com>
2023-06-21 18:47:19 -0400

    Fix execution result check for blocks without seals (#38)
    Fix execution result check to ensure blocks with no seals still get indexed

--- state/convert_test.go
+++ state/convert_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/rosetta/access"
 	"github.com/onflow/rosetta/config"
 	"github.com/stretchr/testify/assert"
@@ -29,6 +30,50 @@ func TestVerifyBlockHash(t *testing.T) {
 			assert.Fail(t, err.Error())
 		}
 		assert.True(t, verifyBlockHash(spork, block.Id, blockHeight, blockHeader, block))
+	}
+}
+
+func TestVerifyExecutionResultHash(t *testing.T) {
+	var startBlockHeight uint64 = 55114464
+	var endBlockHeight uint64 = 55114465
+	ctx := context.Background()
+	spork, err := createSpork(ctx)
+	if err != nil {
+		assert.Fail(t, err.Error())
+	}
+	client := spork.AccessNodes.Client()
+	var sealedResults map[string]string
+	for blockHeight := startBlockHeight; blockHeight < endBlockHeight; blockHeight++ {
+		block, err := client.BlockByHeight(ctx, blockHeight)
+		if err != nil {
+			assert.Fail(t, err.Error())
+		}
+		execResult, err := client.ExecutionResultForBlockID(ctx, block.Id)
+		if err != nil {
+			assert.Fail(t, err.Error())
+		}
+		for _, seal := range block.BlockSeals {
+			sealedResults[string(seal.BlockId)] = string(seal.ResultId)
+		}
+		var resultID flow.Identifier
+		var resultIDV5 flow.Identifier
+		exec, ok := convertExecutionResult(block.Id, blockHeight, execResult)
+		if ok {
+			resultID = deriveExecutionResult(spork, exec)
+		}
+		execV5, okV5 := convertExecutionResultV5(block.Id, blockHeight, execResult)
+		if okV5 {
+			resultIDV5 = deriveExecutionResultV5(execV5)
+		}
+		if !ok && !okV5 {
+			assert.Fail(t, "unable to covert from either hash")
+		}
+		sealedResult, foundOk := sealedResults[string(block.Id)]
+		if foundOk {
+			if string(resultID[:]) != sealedResult && string(resultIDV5[:]) != sealedResult {
+				assert.Fail(t, "target error")
+			}
+		}
 	}
 }
 
