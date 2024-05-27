7049b1e378f487c2a3f71efba85ba720fccefea8
Illia Malachyn <illia.malachyn@gmail.com>
2024-05-27 20:18:05 +0300

    Update mainnet spork info for state indexer's convert test

--- state/convert_test.go
+++ state/convert_test.go
@@ -13,13 +13,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-//var accessAddr = "access-001.canary1.nodes.onflow.org:9000"
-//var startBlockHeight uint64 = 59789556
-//var endBlockHeight uint64 = 59789558
-
-var accessAddr = "access-001.mainnet23.nodes.onflow.org:9000"
-var startBlockHeight uint64 = 55114468
-var endBlockHeight uint64 = 55114470
+var accessAddr = "access-001.mainnet24.nodes.onflow.org:9000"
+var startBlockHeight uint64 = 65264620
+var endBlockHeight uint64 = 65264630
 
 func TestVerifyBlockHash(t *testing.T) {
 	// load mainnet config and get blocks exactly as state.go
@@ -136,6 +132,8 @@ func TestDeriveEventsHash(t *testing.T) {
 			eventHashes = append(eventHashes, hash)
 		}
 		var execResult *entities.ExecutionResult
+
+		//TODO: ILLIA: chunk[0].EventCollection is empty! but should be equal to hash?
 		execResult, err = client.ExecutionResultForBlockID(ctx, block.Id)
 		require.NoError(t, err)
 		require.Equal(t, len(eventHashes), len(execResult.Chunks))
@@ -149,12 +147,11 @@ func TestDeriveEventsHash(t *testing.T) {
 func createSpork(ctx context.Context) (*config.Spork, error) {
 	addr := accessAddr
 	pool := access.New(ctx, []access.NodeConfig{{Address: addr}}, nil)
-	//chain := &config.Chain{Network: "canary"}
 	chain := &config.Chain{Network: "mainnet"}
 	return &config.Spork{
-		Version:     5,
+		Version:     6,
 		Chain:       chain,
 		AccessNodes: pool,
-		RootBlock:   55114467,
+		RootBlock:   65264619,
 	}, nil
 }
